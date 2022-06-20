c16f824df4c9a89ec149da55371d865e043795b6
Sravan Reddy <68369365+sravanreddy005@users.noreply.github.com>
2022-06-20 12:30:07 +0530

    Update common.js
    token updated

--- server/helpers/common.js
+++ server/helpers/common.js
@@ -204,7 +204,7 @@ module.exports.sendWhatsappMessage = (toMobileNo, template) => {
             //     'Content-Type': 'application/json'
             // };
             let headers = {
-                'Authorization': 'Bearer EAANE47CvoO4BABfHHYwFZBB0zWLlY3qwVkd2wIamK8D3BFrDZA3u80nB2mLL5rHQDM6ZAeZBtveEPVsuS4W476UM5m6ceM0MpS64KS7yAusUPEUwzSGb4TZBdEcTDTgOvWJPwtnIzflDrZApAk56daDRwYG5OTZBMnBVj5mCbAofsjXxTTu9auO',
+                'Authorization': 'Bearer EAANE47CvoO4BAOZC9WGO2mIUjmiGQxEGwGb6yvBaPfoC0AfC27etyJN9yYVro4SYw6RLZBPpZAoW7bJiLffTsyV2f1DBymAr8q53BByCRSxLI8g7DV3q165eMg0jZAZAsZBRK9OLeJzFunonqCENJrFYHps5ZAZAaLPTrYRVkvMreZANU8WGbnsbn',
                 'Content-Type': 'application/json'
             };
             let data = {
