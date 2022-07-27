1e9a62e8bafa5f55f7a1d6f30ed7e9c6ffe7df3f
Sravan <sravanreddy.jukanti@gmail.com>
2022-07-27 21:25:15 +0530

    changed db credentials from config

--- server/database/db-config.js
+++ server/database/db-config.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('ngx_a_test', 'root', '', {
+const sequelize = new Sequelize(process.env.MYSQL_DB_NAME, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
   host: process.env.MYSQL_HOST,
   dialect: 'mysql',
   logging: false,
