730b77584841fb474accaad5897faacaaced41ff
Sravan <sravanreddy.jukanti@gmail.com>
2022-09-25 16:10:58 +0530

    cc

--- server/app.js
+++ server/app.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
     next();
 });
 
-// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/web', webRoutes);
